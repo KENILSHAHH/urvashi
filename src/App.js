dfbe6ae7e739290a47a57b896fb8670bc37640ad
Kenil shah <kenilshah1505@gmail.com>
2023-10-05 00:09:13 +0530

    sorry urvashi thoda laptop busy tha

--- src/App.js
+++ src/App.js
@@ -1,45 +1,62 @@
-import React, { useEffect, useState } from 'react'
+/** @format */
+
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const[data,setData]=useState();
-  const[tempData,setTempData]=useState();
-
-  const getData =()=>{
+  const [data, setData] = useState();
+  const [tempData, setTempData] = useState();
+  const [string, setString] = useState('');
+  const getData = () => {
     fetch('celebrities.json')
-    .then((response)=>{
-      return response.json()
-    })
-    .then((data)=>{
-      setData(data);
-      setTempData(data);
-    })
-  }
-  useEffect(()=>{
-    getData()
-  },[]);
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        setTempData(data);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+  const handleFieldChange = (index, field, value) => {
+    // Create a new array with the updated JSON object
+    const updatedArray = tempData.map((item, i) => {
+      if (i === index) {
+        return { ...item, [field]: value };
+      }
+      return item;
+    });
 
+    // Update the state with the new array
+    setTempData(updatedArray);
+  };
 
-const handleSave=(e)=>{
-  e.preventDefault();
-}
+  const handleSave = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="App">
       <h1>Displaying data</h1>
       <form onSubmit={handleSave}>
-       
-      { tempData && tempData.length >0 && tempData.map((user,key)=>{
-        return (
-       <input type='text' name='country' value={ user.country}/>
-        )
-      })}
-      { data && data.length >0 && data.map((user,key)=>{
-        return (
-       <input type='text' name='first' value={ user.first}/>
-        )
-      })}
-      <button>Cancel</button>
-      <button type='submit'>Save</button>
+        {tempData &&
+          tempData.length > 0 &&
+          tempData.map((user, key) => {
+            return (
+              <input
+                type="text"
+                
+                onChange={(e) =>
+                  handleFieldChange(key,"country" , e.target.value)
+                }
+                value={user.country}
+              />
+            );
+          })}
+
+        <button>Cancel</button>
+        <button type="submit">Save</button>
       </form>
     </div>
   );
